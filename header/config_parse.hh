75cf0d49cb0b056be51419e2d79926f54e5feea3
Chris Samuelson <chris.sam55@gmail.com>
2017-03-28 12:21:56 -0400

    Moved implementation to config parsing class

--- header/config_parse.hh
+++ header/config_parse.hh
@@ -4,6 +4,7 @@
 #include<string>
 #include<map>
 #include<fstream>
+#include<regex>
 
 class config_parse{
 private:
@@ -14,11 +15,47 @@ private:
 public:
   void add_element( const std::string& arg, const std::string& eleName ){
     mElements[arg] = element();
-    mElements[arg][eleName] = string();
+    mElements[arg][eleName] = std::string();
   }
 
-  template<typename stream>
-  void parse_config( stream&& file ){
+  template<typename T>
+  T get_element( const std::string& tag, const std::string& name ){
+    return T( mElements[tag][name] );
+  }
+
+  void parse_config( const std::string& fileName ){
+    std::regex rexp1( "\\[(\\w+)\\]((?:\\s*\\w+\\s*=\\s*\\w+\\s*)+)" );
+    std::regex rexp2( "\\s*(\\w+)\\s*=\\s*(\\w+)\\s*" );
+    std::fstream file( fileName );
+    std::string text;
+    std::smatch matches1;
+    std::smatch matches2;
+
+    file.seekg( 0, std::ios::end );
+    text.reserve( file.tellg() );
+    file.seekg( 0, std::ios::beg );
+
+    text.assign( std::istreambuf_iterator<char>( file ),
+                 std::istreambuf_iterator<char>() );
+
+    while( regex_search( text, matches1, rexp1 ) ){
+      for( unsigned int i = 1; i < matches1.size(); ){
+        std::string tag = matches1[i++];
+        std::string values = matches1[i++];
+
+        while( std::regex_search( values, matches2, rexp2 ) ){
+          unsigned int j = 1;
+          while( j < matches2.size() ){
+            mElements[tag][matches2[j]] = matches2[j + 1];
+            j += 3;
+          }
+
+          values = matches2.suffix().str();
+        }
+      }
+
+      text = matches1.suffix().str();
+    }
   }
 };
 
--- source/main.cc
+++ source/main.cc
@@ -1,48 +1,19 @@
-#include<fstream>
-
-//#include"config_parse.hh"
-
-#include<regex>
 #include<string>
 #include<iostream>
+
+#include"config_parse.hh"
 
 using namespace std;
 
 int main(){
-/*  configparse cp;
-  fstream file( "data/config.ini" );
-
+  config_parse cp;
   cp.add_element( "CPU", "ports" );
   cp.add_element( "CPU", "timers" );
   cp.add_element( "CPU", "adcs" );
 
-  file = cp.parse_config( file );*/
-  string text( "[aoeu]\n\ta = 1\n\tb = 2\n\tstuff = things\n[htns]\n\tval = text\n" );
-  regex rexp1( "\\[(\\w+)\\]((?:\\s*\\w+\\s*=\\s*\\w+\\s*)+)" );
-  regex rexp2( "\\s*(\\w+)\\s*=\\s*(\\w+)\\s*" );
-  smatch matches1;
-  smatch matches2;
+  cp.parse_config( "data/config.ini" );
 
-  while( regex_search( text, matches1, rexp1 ) ){
-    for( unsigned int i = 1; i < matches1.size(); ){
-      string match = matches1[i++];
-      string values = matches1[i++];
-
-      cout << "TAG" << endl;
-      cout << match << endl;
-      cout << "VALS" << endl;
-      cout << values << endl;
-
-      while( regex_search( values, matches2, rexp2 ) ){
-        for( unsigned int j = 0; j < matches2.size(); ++j ){
-          cout << j << endl;
-          cout << '\t' << matches2[j] << endl;
-        }
-        values = matches2.suffix().str();
-      }
-    }
-    text = matches1.suffix().str();
-  }
+  cout << cp.get_element<string>( "CPU", "ports" ) << endl;
 
   return 0;
 }
