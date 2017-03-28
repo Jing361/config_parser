56dacc80fd3fe6a43dbc9f1cbdb7f49b269fbe6b
Chris Samuelson <chris.sam55@gmail.com>
2017-03-28 11:20:40 -0400

    Found regex settings/usage to extract interesting data

--- source/main.cc
+++ source/main.cc
@@ -17,28 +17,31 @@ int main(){
   cp.add_element( "CPU", "adcs" );
 
   file = cp.parse_config( file );*/
-  string text( "[aoeu]\n\ta = 1\n\tb = 2\n[htns]\n\tval = text\n" );
-  regex rexp1( "\\[(\\w+)\\]((?:\\s*\\w+\\s*=\\s*\\w+)+)" );
-  regex rexp2( "(?:\\s*(?:(\\w+)\\s*=\\s*(\\w+))+)" );
+  string text( "[aoeu]\n\ta = 1\n\tb = 2\n\tstuff = things\n[htns]\n\tval = text\n" );
+  regex rexp1( "\\[(\\w+)\\]((?:\\s*\\w+\\s*=\\s*\\w+\\s*)+)" );
+  regex rexp2( "\\s*(\\w+)\\s*=\\s*(\\w+)\\s*" );
   smatch matches1;
   smatch matches2;
 
-  regex_search( text, matches1, rexp1 );
+  while( regex_search( text, matches1, rexp1 ) ){
+    for( unsigned int i = 1; i < matches1.size(); ){
+      string match = matches1[i++];
+      string values = matches1[i++];
 
-  for( unsigned int i = 1; i < matches1.size(); ){
-    string match = matches1[i++];
-    string values = matches1[i++];
+      cout << "TAG" << endl;
+      cout << match << endl;
+      cout << "VALS" << endl;
+      cout << values << endl;
 
-    cout << "TAG" << endl;
-    cout << match << endl;
-    cout << "VALS" << endl;
-    cout << values << endl;
-
-    regex_search( values, matches2, rexp2 );
-    for( unsigned int j = 0; j < matches2.size(); ++j ){
-      cout << j << endl;
-      cout << '\t' << matches2[j] << endl;
+      while( regex_search( values, matches2, rexp2 ) ){
+        for( unsigned int j = 0; j < matches2.size(); ++j ){
+          cout << j << endl;
+          cout << '\t' << matches2[j] << endl;
+        }
+        values = matches2.suffix().str();
+      }
     }
+    text = matches1.suffix().str();
   }
 
   return 0;
