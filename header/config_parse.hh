4a63d5035dd214610652928d7ad7340e8afbb77f
Chris Samuelson <chris.sam55@gmail.com>
2017-06-14 13:47:24 -0400

    Mostly added doc-blocks, increased a lil efficiency
    In element retrieval, save data on first retrieval attempt, instead of
    checking for existence, and after existence confirmed, then finding the data
    again to use it.  Now concurrent with existence check, save the data.

--- header/config_parse.hh
+++ header/config_parse.hh
@@ -16,6 +16,31 @@ public:
   const char* what() const noexcept override;
 };
 
+/*! Configuration file parser
+ *
+ * Data to be provided via an ini file can be described using add_element
+ * function calls.  The file can then be parsed using parse_config, and
+ * finally data retrieved via get_element.
+ *
+ * Configuration is described by add_element.  The first argument to add_element
+ * describes a word to be found between square brackets, [].  The second
+ * argument describes pieces found under that tag, using an = sign.  i.e.:
+ * 
+ * [FOO]
+ *   bar = test 
+ *   value = 42 
+ *
+ * The above would be described as:
+ * argparse ap;
+ *
+ * ap.add_element( "FOO", "bar" );
+ * ap.add_element( "FOO", "value" );
+ *
+ * ap.parse_config( "filename.ini" );
+ *
+ * assert( ap.get_element<string>( "FOO", "bar" ) == "test" );
+ * assert( ap.get_element<int>( "FOO", "value" ) == 42 );
+ */
 class config_parse{
 private:
   typedef std::map<std::string, std::string> element;
@@ -23,17 +48,38 @@ private:
   std::map<std::string, element> mElements;
 
 public:
+  /*! Provide names of config data to look for
+   *
+   * @param arg  Tag name to look for between square brackets
+   *
+   * @param eleName  Element to look for under specified tag
+   *
+   */
   void add_element( const std::string& arg, const std::string& eleName );
 
+  /*! Retrieve element from parsed data
+   *
+   * @tparam T  Type data should be translated into via stream extraction
+   *
+   * @param tag  Name of tag (value in square brackets) to get data from
+   *
+   * @param name  Name of element under specified tag
+   *
+   * Throws an error if configuration at tag and name were not declared.  Data
+   * t of type T is created via default constructor, and then data is put into t
+   * using stream extraction.
+   */
   template<typename T>
   T get_element( const std::string& tag, const std::string& name ){
+    std::string text;
+
     try{
-      mElements.at( tag ).at( name );
-    } catch( std::out_of_range){
+      text = mElements.at( tag ).at( name );
+    } catch( std::out_of_range ){
       throw element_not_found( tag, name );
     }
 
-    std::stringstream ss( mElements[tag][name] );
+    std::stringstream ss( text );
     T t;
 
     ss >> t;
@@ -41,6 +87,13 @@ public:
     return t;
   }
 
+  /*! Parse file for config previously specified elements
+   *
+   * @param fileName  Name of file to be parsed
+   *
+   * Searches for data specified inside fileName file that was described using
+   * add_element function
+   */
   void parse_config( const std::string& fileName );
 };
 
